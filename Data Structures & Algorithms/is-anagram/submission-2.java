b715cbfb2d641bcf1fcd0c8ed18ce7c4b0fe25ee
schauhan2208 <schauhan2208@gmail.com>
2026-07-29 10:30:44 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> charMap = new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            charMap.put(c, charMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            if(!charMap.containsKey(c)){
+                return false;
+            }
+            charMap.put(c, charMap.get(c) - 1);
+
+            if(charMap.get(c) == 0){
+                charMap.remove(c);
+            }
+        }
+
+        return charMap.isEmpty();
+    }    
+}
