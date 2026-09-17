104e7611d9a6cdc7418e4eef5a4c0946126b0ccc
schauhan2208 <schauhan2208@gmail.com>
2026-09-17 13:25:19 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        while  '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()',"")
+            s = s.replace('{}',"")
+            s = s.replace('[]',"")
+        return len(s) == 0
+
+
+
+        
